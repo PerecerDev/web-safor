95d628533f8cd929f83fe48b415895a1e1c8583f
Alex <alexperecer@gmail.com>
2023-09-26 20:48:24 +0200

    slogan block

---
+++ src/styleConstants.js
@@ -0,0 +1,55 @@
+/*------ NORMAL STATES -------*/
+
+// Colors
+export const COLOR_PRIMARY = "text-amber-600 ";
+export const COLOR_SECONDARY = "text-yellow-950 ";
+export const COLOR_TEXT = "text-gray-600 ";
+export const COLOR_TITLES = "text-orange-500 ";
+export const COLOR_SUBTITLES = "text-white ";
+export const COLOR_BUTTON_TEXT = "text-black ";
+export const COLOR_CTA_TEXT = "text-white ";
+
+// Gradients
+export const GRADIENT_PRIMARY = "bg-gradient-to-r from-amber-600 to-amber-700 ";
+export const GRADIENT_SECONDARY = "bg-gradient-to-r from-amber-800 to-amber-950 ";
+
+// Fonts
+export const FONT_TITLES = "font-serif ";
+export const FONT_SUBTITLES = "font-lato ";
+export const FONT_TEXT = "font-lato ";
+
+// Font Sizes
+export const SIZE_TITLES = "text-7xl ";
+export const SIZE_SUBTITLES = "text-2xl ";
+export const SIZE_TEXT = "text-base ";
+export const SIZE_BUTTONS = "text-lg ";
+export const SIZE_MENUS = "text-md ";
+
+// Font Weight
+export const TEXT_THIN = "font-thin ";
+export const TEXT_BOLD = "font-bold ";
+
+// Borders
+export const BORDER_BUTTONS = "border-2 border-gray-400 ";
+export const BORDER_BLOCKS = "border border-gray-300 ";
+
+// Radius
+export const RADIUS_BLOCK = "rounded-md ";
+export const RADIUS_BUTTON = "rounded-lg ";
+
+// Shadows & Glow
+export const SHADOW_BLOCKS = "shadow-md ";
+export const SHADOW_BUTTONS = "shadow-lg ";
+export const GLOW_EFFECT = "shadow-md shadow-amber-700 shadow-opacity-30 ring-1 ring-amber-700 ring-opacity-30 ";
+
+// Gap
+export const GAP_BLOCKS = "gap-4 ";
+
+// Display & Positioning
+export const HOVER_MOVE_UP = "hover:-translate-y-0.5 hover:transform ";
+
+// Transitions & Animations
+export const TRANSITION_DEFAULT = "transition duration-300 ";
+export const TRANSITION_FAST = "transition duration-150 ";
+export const TRANSITION_SLOW = "transition duration-500 ";
+
